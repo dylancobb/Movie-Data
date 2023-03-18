f64616194ca86e092f15832965a7d200ac17e792
Hex <hexmusictheory@protonmail.com>
2023-03-18 00:41:25 +0000

    feat: add display panel for movie data

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 - An interactable list of movies will appear beneath this panel, with buttons to move between different items in the list, create items, delete items, and bring up another panel to edit the data of a given movie.
 - Aesthetically, some kind of banner and footer might be nice, but given there is a lot of data to display they should be small/unobtrusive enough that all or most of the data can fit on a desktop screen, and not too much scrolling is needed on phone displays.
 # Building
+- Decided on page colour scheme, logo, typography and created the header
+- Created the main panel that will display the movie data and populated it with the first movie from the movieData object as dummy text.
 # Debugging
+- I tried to implement a `<meter>` element for the score, and wasted time fiddling with it. In the end, a simple number looks best.
--- scripts.js
+++ scripts.js
@@ -1,35 +1,47 @@
 let movieData = {
     "The Darjeeling Limited": {
-      plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
-      cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
-      runtime: 151,
-      rating: 7.2,
-      year: 2007,
+        plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+        cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+        runtime: 151,
+        rating: 7.2,
+        year: 2007,
     },
     "The Royal Tenenbaums": {
-      plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
-      rating: 7.6,
-      year: 2001,
-      cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
-      runtime: 170,
+        plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+        rating: 7.6,
+        year: 2001,
+        cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+        runtime: 170,
     },
     "Fantastic Mr. Fox": {
-      year: 2009,
-      plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
-      cast: [
-        "George Clooney",
-        "Meryl Streep",
-        "Bill Murray",
-        "Jason Schwartzman",
-      ],
-      runtime: 147,
-      rating: 7.9,
+        year: 2009,
+        plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+        cast: [
+            "George Clooney",
+            "Meryl Streep",
+            "Bill Murray",
+            "Jason Schwartzman",
+        ],
+        runtime: 147,
+        rating: 7.9,
     },
     "The Grand Budapest Hotel": {
-      rating: 8.1,
-      runtime: 159,
-      year: 2014,
-      plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
-      cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+        rating: 8.1,
+        runtime: 159,
+        year: 2014,
+        plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+        cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
     },
-  };
+};
+
+let keys = Object.keys(movieData);
+let movieTitle = document.getElementById("movie-title");
+let moviePlot = document.getElementById("movie-plot");
+let movieRuntime = document.getElementById("movie-runtime");
+let movieRating = document.getElementById("movie-rating");
+let movieCast = document.getElementById("movie-cast");
+movieTitle.innerHTML = `${keys[0]} (${movieData[keys[0]].year})`;
+moviePlot.innerHTML = movieData[keys[0]].plot;
+movieRuntime.innerHTML = `<span class="data-heading">Runtime:</span> ${movieData[keys[0]].runtime} minutes`;
+movieRating.innerHTML = `<span class="data-heading">Rating</span>: ${movieData[keys[0]].rating}`;
+movieCast.innerHTML = `<span class="data-heading">Starring:</span> ${movieData[keys[0]].cast}`;
